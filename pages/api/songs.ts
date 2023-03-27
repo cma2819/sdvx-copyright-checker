91898c61887f4ea1502f4007dc479736db90051d
Cma <chicchaima@gmail.com>
2023-03-28 01:34:30 +0900

    fix

--- pages/api/songs.ts
+++ pages/api/songs.ts
@@ -1,5 +1,5 @@
 import { NextApiHandler } from 'next'
-import { readSongs, SongStore } from '../store/song.store';
+import { readSongs, SongStore } from '../../store/song.store';
 
 const handler: NextApiHandler<SongStore> = (req, res) => {
   res.status(200).json(readSongs());
